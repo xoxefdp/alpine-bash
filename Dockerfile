7e805b5cfa6b1e28e5ff023b569af1c465b8ff8a
Jose Diaz <xoxefdp@gmail.com>
2022-04-05 13:36:28 +0200

    renamed script, Update docker file

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
+ARG ARCH=
 ARG BASH_VERSION
 ARG ALPINE_VERSION
 ARG IMAGE_VERSION=$BASH_VERSION-alpine$ALPINE_VERSION
 
-FROM bash:${IMAGE_VERSION}
+FROM ${ARCH}bash:${IMAGE_VERSION}
 
 # Install system dependencies
 RUN set -xe && \
   apk add --no-cache --update && \
   rm -rf /var/cache/apk/* /tmp/*
-
-# Command to run
-# ENTRYPOINT ["/bin/bash"]
-CMD [ "/usr/local/bin/bash" ]
--- gen-images.sh
+++ gen-images.sh
@@ -6,6 +6,11 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+# PLATFORMS
+PC="linux/amd64,linux/i386"
+ARM="linux/arm64/v8,linux/arm/v7,linux/arm/v6"
+FULL=$PC","$ARM
+
 printHelp() {
   printf "${NC}[*] ${GREEN}USAGE ${NC}\n"
   printf "${NC}[*] ${NC} \t Generates alpine with bash images ${NC}\n"
@@ -18,11 +23,11 @@ printHelp() {
   printf "${NC}[*] ${YELLOW} \t --help                      ${NC}Shows this help message ${NC}\n"
   printf "${NC}[*] ${GREEN}EXAMPLES ${NC}\n"
   printf "${NC}[*] ${NC} \t Generate image for alpine with bash in specified version for bash and alpine \n"
-  printf "${NC}[*] ${YELLOW} \t\t $ ./generate.sh --registry-user USER --image-name IMAGE --bash-version BASH_VERSION --alpine-version ALPINE_VERSION \n"
+  printf "${NC}[*] ${YELLOW} \t\t $ ./gen-images.sh --registry-user USER --image-name IMAGE --bash-version BASH_VERSION --alpine-version ALPINE_VERSION \n"
   printf "${NC}[*] ${NC} \t Generate image for alpine with bash in specified image version \n"
-  printf "${NC}[*] ${YELLOW} \t\t $ ./generate.sh --registry-user USER --image-name IMAGE --image-version VERSION \n"
+  printf "${NC}[*] ${YELLOW} \t\t $ ./gen-images.sh --registry-user USER --image-name IMAGE --image-version VERSION \n"
   printf "${NC}[*] ${NC} \t Generate image for alpine with bash in latest version \n"
-  printf "${NC}[*] ${YELLOW} \t\t $ ./generate.sh --registry-user USER --image-name IMAGE \n"
+  printf "${NC}[*] ${YELLOW} \t\t $ ./gen-images.sh --registry-user USER --image-name IMAGE \n"
 }
 
 while [[ $# -gt 0 ]]
@@ -35,6 +40,10 @@ case $key in
   ;;
   --image-name)
   IMAGE_NAME="$2"
+  shift
+  ;;
+  --platforms-list)
+  PLATFORMS_LIST="$2"
   shift
   ;;
   --bash-version)
@@ -56,25 +65,38 @@ esac
 shift
 done
 
-if [[ -n $REGISTRY_USER && -n $IMAGE_NAME ]]; then
+PLATFORMS=""
+
+if [[ -n $REGISTRY_USER && -n $IMAGE_NAME && -n $PLATFORMS_LIST ]]; then
+  if [[ $PLATFORMS_LIST == "pc" ]]; then
+    PLATFORMS=$PC
+  elif [[ $PLATFORMS_LIST == "arm" ]]; then
+    PLATFORMS=$ARM
+  elif [[ $PLATFORMS_LIST == "full" ]]; then
+    PLATFORMS=$FULL
+  else
+    printf "${YELLOW}[*] ${RED}Param --platforms-list should be 'pc', 'arm' or 'full' ${NC}\n"
+    exit 1
+  fi
+
   if [[ -n $BASH_VERSION && -n $ALPINE_VERSION ]]; then
     printf "${YELLOW}[*] ${GREEN}Generating image with bash $BASH_VERSION and alpine $ALPINE_VERSION version ${NC}\n"
-    # docker build --build-arg ALPINE_VERSION=$VERSION -t alpine-bash:$VERSION .
-    docker buildx build --build-arg BASH_VERSION=$BASH_VERSION --build-arg ALPINE_VERSION=$ALPINE_VERSION -t $REGISTRY_USER/$IMAGE_NAME:$BASH_VERSION-alpine$ALPINE_VERSION --push .
+    # docker build --build-arg BASH_VERSION=$BASH_VERSION --build-arg ALPINE_VERSION=$ALPINE_VERSION -t $REGISTRY_USER/$IMAGE_NAME:$BASH_VERSION-alpine$ALPINE_VERSION .
+    docker buildx build --platform $PLATFORMS --build-arg BASH_VERSION=$BASH_VERSION --build-arg ALPINE_VERSION=$ALPINE_VERSION -t $REGISTRY_USER/$IMAGE_NAME:$BASH_VERSION-alpine$ALPINE_VERSION --push .
     printf "${YELLOW}[*] ${GREEN}Finished image for bash $BASH_VERSION and alpine $ALPINE_VERSION versions ${NC}\n"
   elif [[ -n $IMAGE_VERSION ]]; then
     printf "${YELLOW}[*] ${GREEN}Generating image with bash and alpine with $IMAGE_VERSION version ${NC}\n"
-    # docker build -t alpine-bash:latest .
-    docker buildx build --build-arg IMAGE_VERSION=$IMAGE_VERSION -t $REGISTRY_USER/$IMAGE_NAME:$IMAGE_VERSION --push .
+    # docker build --build-arg IMAGE_VERSION=$IMAGE_VERSION -t $REGISTRY_USER/$IMAGE_NAME:$IMAGE_VERSION .
+    docker buildx build --platform $PLATFORMS --build-arg IMAGE_VERSION=$IMAGE_VERSION -t $REGISTRY_USER/$IMAGE_NAME:$IMAGE_VERSION --push .
     printf "${YELLOW}[*] ${GREEN}Finished image for bash and alpine with $IMAGE_VERSION version ${NC}\n"
   else
     printf "${YELLOW}[*] ${GREEN}Generating image for bash and alpine with latest version ${NC}\n"
-    # printf "${YELLOW}[*] ${RED}Mandatory to launch also with --bash-version and --alpine-version or with --image-version ${NC}\n"
-    docker buildx build --build-arg IMAGE_VERSION=latest -t $REGISTRY_USER/$IMAGE_NAME:latest --push .
+    # docker build -t $REGISTRY_USER/$IMAGE_NAME:latest .
+    docker buildx build --platform $PLATFORMS -t $REGISTRY_USER/$IMAGE_NAME:latest --push .
     printf "${YELLOW}[*] ${GREEN}Finished image for bash and alpine with latest version ${NC}\n"
   fi
   exit 0
 else
-  printf "${YELLOW}[*] ${RED}Mandatory to launch with --registry-user and --image-name ${NC}\n"
+  printf "${YELLOW}[*] ${RED}Mandatory to launch with --registry-user, --image-name and --builder ${NC}\n"
   exit 1
 fi
